3d32c1520519da252ae34f0b4c006dc79b87e07d
Gillian Palhano <gillian.palhano@copel.com>
2024-07-15 17:10:02 -0300

    alterado formatacao automatica

--- out/extension.js
+++ out/extension.js
@@ -34,7 +34,8 @@ function activate(context) {
             const text = document.getText();
             const lines = text.split(/\r?\n/);
             // Encontra o comprimento máximo da chave
-            let maxKeyLength = 0;
+            // let maxKeyLength = 0; // v0.1.0
+            let maxKeyLength = 20; // tamanho minimo
             lines.forEach(line => {
                 const match = line.match(/^\s*([A-Za-z0-9_\-]+)\s*=\s*(.*)$/);
                 if (match) {
@@ -50,8 +51,10 @@ function activate(context) {
                 if (match) {
                     const key = match[1].trim();
                     const value = match[2].trim();
-                    const spacesToAdd = maxKeyLength - key.length + 10; // Calcula os espaços a adicionar
-                    const formattedLine = `    ${key}= ${' '.repeat(spacesToAdd)}${value}`;
+                    // const spacesToAdd = maxKeyLength - key.length + 10; // Calcula os espaços a adicionar  // v0.10
+                    // const formattedLine = `    ${key}= ${' '.repeat(spacesToAdd)}${value}`; // v0.10
+                    const spacesToAdd = maxKeyLength - key.length; // Calcula os espaços a adicionar  // v0.10
+                    const formattedLine = `${' '.repeat(spacesToAdd)}${key}=  ${value}`;
                     edits.push(vscode.TextEdit.replace(new vscode.Range(index, 0, index, line.length), formattedLine));
                 }
             });
--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,8 @@ export function activate(context: vscode.ExtensionContext) {
       const lines = text.split(/\r?\n/);
 
       // Encontra o comprimento máximo da chave
-      let maxKeyLength = 0;
+      // let maxKeyLength = 0; // v0.1.0
+      let maxKeyLength = 20; // tamanho minimo
       lines.forEach(line => {
         const match = line.match(/^\s*([A-Za-z0-9_\-]+)\s*=\s*(.*)$/);
         if (match) {
@@ -26,8 +27,10 @@ export function activate(context: vscode.ExtensionContext) {
         if (match) {
           const key = match[1].trim();
           const value = match[2].trim();
-          const spacesToAdd = maxKeyLength - key.length + 10; // Calcula os espaços a adicionar
-          const formattedLine = `    ${key}= ${' '.repeat(spacesToAdd)}${value}`;
+          // const spacesToAdd = maxKeyLength - key.length + 10; // Calcula os espaços a adicionar  // v0.10
+          // const formattedLine = `    ${key}= ${' '.repeat(spacesToAdd)}${value}`; // v0.10
+          const spacesToAdd = maxKeyLength - key.length; // Calcula os espaços a adicionar  // v0.10
+          const formattedLine = `${' '.repeat(spacesToAdd)}${key}=  ${value}`;
           edits.push(vscode.TextEdit.replace(new vscode.Range(index, 0, index, line.length), formattedLine));
         }
       });
